fa91611dda4475f3c4c0bffd39fdbe06d87c3129
Kiyong Kim <scgyong@kpu.ac.kr>
2018-09-09 00:52:09 +0900

    color change, random color, width change

--- pr0911_tree/tree.py
+++ pr0911_tree/tree.py
@@ -3,19 +3,25 @@ import random
 
 MAX_LEVEL = 7
 
-COLORS = [ (80, 223, 80), (80, 193, 80), (80, 52, 26), (65, 42, 21), (50, 32, 16), (35, 22, 11), (20, 12, 6) ]
+COLORS = [ (80, 223, 80), (80, 193, 80), (80, 52, 36), (65, 42, 31), (50, 32, 26), (35, 22, 21), (20, 12, 16) ]
+
+def adjustColorValue(v):
+	v = int(v * random.uniform(0.8, 1.2))
+	if v > 255: v = 255
+	return v
 
 def setColor(level):
-	turtle.pencolor(COLORS[level - 1])
+	(r, g, b) = COLORS[level - 1]
+	turtle.pencolor((adjustColorValue(r), adjustColorValue(g), adjustColorValue(b)))
 
 # 21, 12, 6
 # 83, 49, 24
 
 def tree(level):
 	length = level * random.uniform(17, 23)
-	turtle.width(level * 2)
+	turtle.width(level * 2 - 1)
 	setColor(level)
-	# turtle.pendown()
+	turtle.pendown()
 	turtle.forward(length)
 	if (level > 1):
 		left = random.uniform(15, 25)
@@ -25,8 +31,8 @@ def tree(level):
 		turtle.right(left + right)
 		tree(level - 1)
 		turtle.left(right)
-	# turtle.penup()
-	setColor(level)
+	turtle.penup()
+	# setColor(level)
 	turtle.backward(length)
 
 turtle.speed(0)
