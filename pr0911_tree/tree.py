9e2c2dd2191f541067f8df4e2ad8f238741dce43
Kiyong Kim <scgyong@kpu.ac.kr>
2018-09-09 00:36:33 +0900

    set predefined color

--- pr0911_tree/tree.py
+++ pr0911_tree/tree.py
@@ -2,9 +2,18 @@ import turtle
 
 MAX_LEVEL = 7
 
+COLORS = [ (80, 223, 80), (80, 193, 80), (80, 52, 26), (65, 42, 21), (50, 32, 16), (35, 22, 11), (20, 12, 6) ]
+
+def setColor(level):
+	turtle.pencolor(COLORS[level - 1])
+
+# 21, 12, 6
+# 83, 49, 24
+
 def tree(level):
 	length = level * 20
 	turtle.width(level * 2)
+	setColor(level)
 	turtle.forward(length)
 	if (level > 1):
 		turtle.left(20)
@@ -19,6 +28,7 @@ turtle.penup()
 turtle.left(90)
 turtle.backward(300)
 turtle.pendown()
+turtle.colormode(255)
 
 tree(MAX_LEVEL)
 
