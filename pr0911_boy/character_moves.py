1063a4f73f18c44f7900e2bad34b6b4fb40a12dd
Kiyong Kim <scgyong@kpu.ac.kr>
2018-09-14 16:16:31 +0900

    _now 가 없는 버전으로 수정

--- pr0911_boy/character_moves.py
+++ pr0911_boy/character_moves.py
@@ -7,9 +7,10 @@ character = load_image('character.png')
 
 x = 0
 while (x < 800):
-	clear_canvas_now()
-	grass.draw_now(400, 30)
-	character.draw_now(x, 90)
+	clear_canvas()
+	grass.draw(400, 30)
+	character.draw(x, 90)
+	update_canvas()
 	x = x + 2
 	delay(0.01)
 
