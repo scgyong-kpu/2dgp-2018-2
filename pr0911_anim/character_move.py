4ef05f24fb6bb2e11521d79aeb69f72e8f37c6c8
Kiyong Kim <scgyong@kpu.ac.kr>
2018-09-14 16:59:56 +0900

    run animation

--- pr0911_anim/character_move.py
+++ pr0911_anim/character_move.py
@@ -1,4 +1,22 @@
-import turtle
+from pico2d import *
 
-turtle.forward(100)
-turtle.exitonclick()
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('run_animation.png')
+
+frame = 0
+x = 0
+while (x < 800):
+    clear_canvas()
+    grass.draw(400, 30)
+    character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+    frame = frame + 1
+    if (frame >= 8): frame = 0
+    update_canvas()
+    x = x + 2
+    delay(0.05)
+
+# fill here
+
+close_canvas()
