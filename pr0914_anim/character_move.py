ed89f5e63d229024a9256acad2fd12c80fc922a2
Kiyong Kim <scgyong@kpu.ac.kr>
2018-09-17 12:30:21 +0900

    Phase 4종 모두 추가.

--- pr0914_anim/character_move.py
+++ pr0914_anim/character_move.py
@@ -20,7 +20,21 @@ while True:
             phase = 'up'
     elif phase == 'up':
         y += speed
-    print(x, y)
+        if y >= 550:
+            y = 550
+            phase = 'left'
+    elif phase == 'left':
+        x -= speed
+        if x < 50:
+            x = 50
+            phase = 'down'
+    elif phase == 'down':
+        y -= speed
+        if y < 90:
+            y = 90
+            phase = 'right'
+
+    # print(x, y)
     character.clip_draw(frame * 100, 0, 100, 100, x, y)
     update_canvas()
     frame = (frame + 1) % 8
