561f0f3da89d1b31649cf4b0d4bb741d9ff9cd4f
Kiyong Kim <scgyong@kpu.ac.kr>
2018-09-17 12:22:15 +0900

    무한루프로 변경하고 y 변수 및 speed 변수 신설.

--- pr0914_anim/character_move.py
+++ pr0914_anim/character_move.py
@@ -6,14 +6,16 @@ grass = load_image('grass.png')
 character = load_image('run_animation.png')
 
 frame = 0
-x = 0
-while x < 800:
+x, y = 0, 90
+speed = 2
+
+while True:
     clear_canvas()
     grass.draw(400, 30)
-    character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+    character.clip_draw(frame * 100, 0, 100, 100, x, y)
     frame = (frame + 1) % 8
     update_canvas()
-    x = x + 2
+    x = x + speed
     delay(0.05)
     get_events()
 
