a51e74eb345d3eb32518d41dae22ce2c099d7ff9
Kiyong Kim <scgyong@kpu.ac.kr>
2018-09-21 15:14:30 +0900

    waypoint 추가.

--- pr0918_key/character_moves.py
+++ pr0918_key/character_moves.py
@@ -4,7 +4,7 @@ speed = 3
 
 def handle_events():
     global running
-    global tx, ty
+    global waypoints
     events = get_events()
     for e in events:
         if e.type == SDL_QUIT:
@@ -12,8 +12,9 @@ def handle_events():
         elif e.type == SDL_KEYDOWN:
             if e.key == SDLK_ESCAPE:
                 running = False
-        elif e.type == SDL_MOUSEMOTION:
+        elif e.type == SDL_MOUSEBUTTONDOWN:
             tx, ty = e.x, 600 - e.y
+            waypoints += [ (tx, ty) ]
 
 open_canvas()
 
@@ -21,8 +22,8 @@ grass = load_image('grass.png')
 character = load_image('run_animation.png')
 
 x, y = 800 // 2, 90
-tx, ty = x, y
-
+# tx, ty = x, y
+waypoints = []
 frame = 0
 running = True
 while running:
@@ -31,21 +32,24 @@ while running:
     character.clip_draw(frame * 100, 0, 100, 100, x, y)
     update_canvas()
     handle_events()
-    dx, dy = tx - x, ty - y
-    dist = math.sqrt(dx ** 2 + dy ** 2)
-    if dist > 0:
-        x += speed * dx / dist
-        y += speed * dy / dist
+    if len(waypoints) > 0:
+        (tx, ty) = waypoints[0]
+        dx, dy = tx - x, ty - y
+        dist = math.sqrt(dx ** 2 + dy ** 2)
+        if dist > 0:
+            x += speed * dx / dist
+            y += speed * dy / dist
 
-        if dx < 0 and x < tx: x = tx
-        if dx > 0 and x > tx: x = tx
-        if dy < 0 and y < ty: y = ty
-        if dy > 0 and y > ty: y = ty
+            if dx < 0 and x < tx: x = tx
+            if dx > 0 and x > tx: x = tx
+            if dy < 0 and y < ty: y = ty
+            if dy > 0 and y > ty: y = ty
 
-    if (x,y) == (tx,ty):
-        hide_cursor()
-    else:
-        show_cursor()
+        if (x,y) == (tx,ty):
+            del waypoints[0]
+            hide_cursor()
+        else:
+            show_cursor()
     frame = (frame + 1) % 8
     delay(0.01)
 
