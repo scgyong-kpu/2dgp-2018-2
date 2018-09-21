013d8b6022681c470b470e01fba5c95d0252f331
Kiyong Kim <scgyong@kpu.ac.kr>
2018-09-21 15:13:13 +0900

    에러해결.

--- pr0918_key/character_moves.py
+++ pr0918_key/character_moves.py
@@ -22,6 +22,7 @@ character = load_image('run_animation.png')
 
 x, y = 800 // 2, 90
 tx, ty = x, y
+
 frame = 0
 running = True
 while running:
@@ -32,13 +33,14 @@ while running:
     handle_events()
     dx, dy = tx - x, ty - y
     dist = math.sqrt(dx ** 2 + dy ** 2)
-    x += speed * dx / dist
-    y += speed * dy / dist
+    if dist > 0:
+        x += speed * dx / dist
+        y += speed * dy / dist
 
-    if dx < 0 and x < tx: x = tx
-    if dx > 0 and x > tx: x = tx
-    if dy < 0 and y < ty: y = ty
-    if dy > 0 and y > ty: y = ty
+        if dx < 0 and x < tx: x = tx
+        if dx > 0 and x > tx: x = tx
+        if dy < 0 and y < ty: y = ty
+        if dy > 0 and y > ty: y = ty
 
     if (x,y) == (tx,ty):
         hide_cursor()
