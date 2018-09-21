a4aa8bb68e5b2b3e91b5198763a11072604fda14
Kiyong Kim <scgyong@kpu.ac.kr>
2018-09-21 16:57:38 +0900

    Grass 와 Boy 를 class 로 만들고 Boy 두개 생성.

---
+++ pr0921_class/main.py
@@ -0,0 +1,53 @@
+from pico2d import *
+
+class Grass:
+	def __init__(self):
+		self.image = load_image('../res/grass.png')
+		print(self.image)
+	def draw(self):
+		self.image.draw(400, 300)
+
+class Boy:
+	def __init__(self):
+		self.x, self.y = 0, 90
+		self.frame = 0
+		self.image = load_image('../res/run_animation.png')
+	def draw(self):
+		self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+	def update(self):
+		self.frame = (self.frame + 1) % 8
+		self.x += 2
+
+def handle_events():
+	global running
+	events = get_events()
+	for e in events:
+		if e.type == SDL_QUIT: 
+			running = False
+		if e.type == SDL_KEYDOWN and e.key == SDLK_ESCAPE:
+			running = False
+
+open_canvas()
+
+g = Grass()
+b = Boy()
+b2 = Boy()
+b2.y = 200
+running = True
+
+
+while running:
+	handle_events()
+	
+	b.update()
+	b2.update()
+
+	clear_canvas()
+	g.draw()
+	b.draw()
+	b2.draw()
+	update_canvas()
+
+	delay(0.03)
+
+
