a8b10fe67e472d416e0aa17ffa7bf4a9b0a1c887
Kiyong Kim <scgyong@kpu.ac.kr>
2018-09-22 18:17:52 +0900

    waypoints 를 각자 가지도록.

--- pr0921_class/main.py
+++ pr0921_class/main.py
@@ -15,13 +15,14 @@ class Boy:
         self.y = random.randint(90, 550)
         self.speed = random.uniform(1.0, 3.0)
         self.frame = random.randint(0, 7)
+        self.waypoints = []
         self.image = load_image('../res/run_animation.png')
     def draw(self):
         self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
     def update(self):
         self.frame = (self.frame + 1) % 8
-        if len(waypoints) > 0:
-            tx, ty = waypoints[0]
+        if len(self.waypoints) > 0:
+            tx, ty = self.waypoints[0]
             dx, dy = tx - self.x, ty - self.y
             dist = math.sqrt(dx ** 2 + dy ** 2)
             if dist > 0:
@@ -34,11 +35,11 @@ class Boy:
                 if dy > 0 and self.y > ty: self.y = ty
 
                 if (tx, ty) == (self.x, self.y):
-                    del waypoints[0]
+                    del self.waypoints[0]
 
 def handle_events():
     global running
-    global waypoints
+    global boys
     events = get_events()
     for e in events:
         if e.type == SDL_QUIT: 
@@ -47,11 +48,12 @@ def handle_events():
             running = False
         if e.type == SDL_MOUSEBUTTONDOWN:
             tx, ty = e.x, 600 - e.y
-            waypoints += [ (tx, ty) ]
+            for b in boys:
+                b.waypoints += [ (tx, ty) ]
 
 
 # tx, ty = 800 // 2, 600 // 2
-waypoints = []
+# waypoints = []
 open_canvas()
 
 g = Grass()
