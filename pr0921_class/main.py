565c060febbe3aba0ce77d99a4a8ce13737fc8dd
Kiyong Kim <scgyong@kpu.ac.kr>
2018-09-22 18:10:01 +0900

    공유 waypoint

--- pr0921_class/main.py
+++ pr0921_class/main.py
@@ -9,43 +9,49 @@ class Grass:
 		self.image.draw(400, 30)
 
 class Boy:
-	def __init__(self):
-		print("Creating..")
-		self.x = random.randint(0, 200)
-		self.y = random.randint(90, 550)
-		self.speed = random.uniform(1.0, 3.0)
-		self.frame = random.randint(0, 7)
-		self.image = load_image('../res/run_animation.png')
-	def draw(self):
-		self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
-	def update(self):
-		self.frame = (self.frame + 1) % 8
-		if self.x < tx:
-			self.x += self.speed
-			if self.x > tx: self.x = tx
-		if self.x > tx:
-			self.x -= self.speed
-			if self.x < tx: self.x = tx
-		if self.y < ty:
-			self.y += self.speed
-			if self.y > ty: self.y = ty
-		if self.y > ty:
-			self.y -= self.speed
-			if self.y < ty: self.y = ty
+    def __init__(self):
+        print("Creating..")
+        self.x = random.randint(0, 200)
+        self.y = random.randint(90, 550)
+        self.speed = random.uniform(1.0, 3.0)
+        self.frame = random.randint(0, 7)
+        self.image = load_image('../res/run_animation.png')
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        if len(waypoints) > 0:
+            tx, ty = waypoints[0]
+            dx, dy = tx - self.x, ty - self.y
+            dist = math.sqrt(dx ** 2 + dy ** 2)
+            if dist > 0:
+                self.x += self.speed * dx / dist
+                self.y += self.speed * dy / dist
+
+                if dx < 0 and self.x < tx: self.x = tx
+                if dx > 0 and self.x > tx: self.x = tx
+                if dy < 0 and self.y < ty: self.y = ty
+                if dy > 0 and self.y > ty: self.y = ty
+
+                if (tx, ty) == (self.x, self.y):
+                    del waypoints[0]
 
 def handle_events():
-	global running
-	global tx, ty
-	events = get_events()
-	for e in events:
-		if e.type == SDL_QUIT: 
-			running = False
-		if e.type == SDL_KEYDOWN and e.key == SDLK_ESCAPE:
-			running = False
-		if e.type == SDL_MOUSEMOTION:
-			tx, ty = e.x, 600 - e.y
+    global running
+    global waypoints
+    events = get_events()
+    for e in events:
+        if e.type == SDL_QUIT: 
+           running = False
+        if e.type == SDL_KEYDOWN and e.key == SDLK_ESCAPE:
+            running = False
+        if e.type == SDL_MOUSEBUTTONDOWN:
+            tx, ty = e.x, 600 - e.y
+            waypoints += [ (tx, ty) ]
 
-tx, ty = 800 // 2, 600 // 2
+
+# tx, ty = 800 // 2, 600 // 2
+waypoints = []
 open_canvas()
 
 g = Grass()
