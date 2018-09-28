8d7101cc8417cc80b1e38b1f764033d78da46343
Kiyong Kim <scgyong@kpu.ac.kr>
2018-09-28 14:24:09 +0900

    __name__ 접근하기

--- pr0928_fw/another.py
+++ pr0928_fw/another.py
@@ -1,3 +1,4 @@
 import show_files
 
 print("Doing somthing")
+print("In another.py, My name is " + __name__)
--- pr0928_fw/show_files.py
+++ pr0928_fw/show_files.py
@@ -4,3 +4,5 @@ filenames = os.listdir()
 for name in filenames:
 	print(name)
 
+print("In show_files.py, My name is " + __name__)
+
