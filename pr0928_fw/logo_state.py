4317cc758f4fb113f4760d390649016ca43d451f
Kiyong Kim <scgyong@kpu.ac.kr>
2018-09-28 17:07:48 +0900

    logo_state 추가

---
+++ pr0928_fw/logo_state.py
@@ -0,0 +1,20 @@
+import game_framework
+
+def enter():
+	pass
+
+def exit():
+	pass
+
+def draw():
+	pass
+
+def update():
+	pass
+
+def pause():
+	pass
+
+def resume():
+	pass
+
--- pr0928_fw/main.py
+++ pr0928_fw/main.py
@@ -1,4 +1,4 @@
 import game_framework
-import boys_state
+import logo_state
 
-game_framework.run(boys_state)
+game_framework.run(logo_state)
