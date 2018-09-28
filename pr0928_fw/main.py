35b3c57e5c897ff7e57433262ece48d47441ad85
Kiyong Kim <scgyong@kpu.ac.kr>
2018-09-28 14:53:27 +0900

    Boy 를 class 로 만든 버전으로 다시 제작.

--- pr0928_fw/main.py
+++ pr0928_fw/main.py
@@ -1,10 +1,50 @@
 from pico2d import *
+import random
 
-speed = 3
+class Grass:
+    def __init__(self):
+        self.image = load_image('../res/grass.png')
+        print(self.image)
+    def draw(self):
+        self.image.draw(400, 30)
 
+class Boy:
+    def __init__(self):
+        print("Creating..")
+        self.x = random.randint(0, 200)
+        self.y = random.randint(90, 550)
+        self.speed = random.uniform(1.0, 3.0)
+        self.frame = random.randint(0, 7)
+        self.waypoints = []
+        self.image = load_image('../res/run_animation.png')
+        self.wp = load_image('../res/wp.png')
+    def draw(self):
+        for wp in self.waypoints:
+            self.wp.draw(wp[0], wp[1])
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        if len(self.waypoints) > 0:
+            tx, ty = self.waypoints[0]
+            dx, dy = tx - self.x, ty - self.y
+            dist = math.sqrt(dx ** 2 + dy ** 2)
+            if dist > 0:
+                self.x += self.speed * dx / dist
+                self.y += self.speed * dy / dist
+
+                if dx < 0 and self.x < tx: self.x = tx
+                if dx > 0 and self.x > tx: self.x = tx
+                if dy < 0 and self.y < ty: self.y = ty
+                if dy > 0 and self.y > ty: self.y = ty
+
+                if (tx, ty) == (self.x, self.y):
+                    del self.waypoints[0]
+
+span = 50
 def handle_events():
     global running
-    global waypoints
+    global boys
+    global span
     events = get_events()
     for e in events:
         if e.type == SDL_QUIT:
@@ -12,25 +52,27 @@ def handle_events():
         elif e.type == SDL_KEYDOWN:
             if e.key == SDLK_ESCAPE:
                 running = False
+            elif e.key in range(SDLK_1, SDLK_9 + 1):
+                span = 20 * (e.key - SDLK_0)
+
         elif e.type == SDL_MOUSEBUTTONDOWN:
-            if e.button == 1:
+            if e.button == SDL_BUTTON_LEFT:
                 tx, ty = e.x, 600 - e.y
-                waypoints += [ (tx, ty) ]
+                for b in boys:
+                    bx = tx + random.randint(-span, span)
+                    by = ty + random.randint(-span, span)
+                    b.waypoints += [ (bx, by) ]
             else:
-                waypoints = []
+                for b in boys:
+                    b.waypoints = []
 
 def enter():
-    global grass, character, wp, x, y, waypoints, frame, running
+    global boys, grass, running
     open_canvas()
 
-    grass = load_image('../res/grass.png')
-    character = load_image('../res/run_animation.png')
-    wp = load_image('../res/wp.png')
+    boys = [ Boy() for i in range(10) ]
+    grass = Grass()
 
-    x, y = 800 // 2, 90
-    # tx, ty = x, y
-    waypoints = []
-    frame = 0
     running = True
 
 def main():
@@ -43,35 +85,17 @@ def main():
     exit()
 
 def draw():
-    global grass, waypoints, wp, character
+    global grass, boys
     clear_canvas()
-    grass.draw(400, 30)
-    for loc in waypoints:
-        wp.draw(loc[0], loc[1])
-    character.clip_draw(frame * 100, 0, 100, 100, x, y)
+    grass.draw()
+    for b in boys:
+        b.draw()
     update_canvas()
 
 def update():
-    global tx, ty, x, y, frame
-    if len(waypoints) > 0:
-        (tx, ty) = waypoints[0]
-        dx, dy = tx - x, ty - y
-        dist = math.sqrt(dx ** 2 + dy ** 2)
-        if dist > 0:
-            x += speed * dx / dist
-            y += speed * dy / dist
-
-            if dx < 0 and x < tx: x = tx
-            if dx > 0 and x > tx: x = tx
-            if dy < 0 and y < ty: y = ty
-            if dy > 0 and y > ty: y = ty
-
-        if (x,y) == (tx,ty):
-            del waypoints[0]
-            # hide_cursor()
-        # else:
-        #     show_cursor()
-    frame = (frame + 1) % 8
+    global boys
+    for b in boys:
+        b.update()
     delay(0.01)
 
 # fill here
