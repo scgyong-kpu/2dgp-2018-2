ce498e70410621caa9e117fa81bf8de59e0b96f6
Kiyong Kim <scgyong@kpu.ac.kr>
2018-10-01 12:42:26 +0900

    boys_state 로 진입

--- pr0928_fw/logo_state.py
+++ pr0928_fw/logo_state.py
@@ -1,12 +1,15 @@
 from pico2d import *
 import game_framework
+import boys_state
 
 def enter():
-	global logo
+	global logo,count
+	count = 0
 	open_canvas()
 	logo = load_image('../res/kpu_credit.png')
 
 def exit():
+	global logo
 	del logo
 	#logo = None
 
@@ -16,7 +19,12 @@ def draw():
 	update_canvas()
 
 def update():
+	global count
+	if (count > 30):
+		game_framework.push_state(boys_state)
+		return
 	delay(0.03)
+	count += 1
 
 def handle_events():
 	pass
