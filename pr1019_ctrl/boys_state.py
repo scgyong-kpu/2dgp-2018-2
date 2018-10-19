e6145c1824ea1277d36806374e59b6bc95795443
Kiyong Kim <scgyong@kpu.ac.kr>
2018-10-19 13:06:16 +0900

    이전 프로젝트로부터 복사

---
+++ pr1019_ctrl/boys_state.py
@@ -0,0 +1,144 @@
+from pico2d import *
+import game_framework
+import random
+import json
+
+# from enum import Enum
+
+BOYS_COUNT = 1000
+
+class Grass:
+    def __init__(self):
+        self.image = load_image('../res/grass.png')
+        print(self.image)
+    def draw(self):
+        self.image.draw(400, 30)
+
+class Boy:
+    image = None
+    wp = None
+    RUN_LEFT, RUN_RIGHT, IDLE_LEFT, IDLE_RIGHT = 0, 1, 2, 3
+    def __init__(self):
+        print("Creating..")
+        # self.state = self.State.s1
+        self.x = random.randint(0, 200)
+        self.y = random.randint(90, 550)
+        self.speed = random.uniform(1.0, 3.0)
+        self.frame = random.randint(0, 7)
+        self.waypoints = []
+        self.state = Boy.IDLE_RIGHT
+        if Boy.image == None:
+            Boy.image = load_image('../res/animation_sheet.png')
+        if Boy.wp == None:
+            Boy.wp = load_image('../res/wp.png')
+    def draw(self):
+        for wp in self.waypoints:
+            Boy.wp.draw(wp[0], wp[1])
+        Boy.image.clip_draw(self.frame * 100, self.state * 100, 100, 100, self.x, self.y)
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        if len(self.waypoints) > 0:
+            tx, ty = self.waypoints[0]
+            dx, dy = tx - self.x, ty - self.y
+            dist = math.sqrt(dx ** 2 + dy ** 2)
+            if dist > 0:
+                self.x += self.speed * dx / dist
+                self.y += self.speed * dy / dist
+
+                if dx < 0 and self.x < tx: self.x = tx
+                if dx > 0 and self.x > tx: self.x = tx
+                if dy < 0 and self.y < ty: self.y = ty
+                if dy > 0 and self.y > ty: self.y = ty
+
+                if (tx, ty) == (self.x, self.y):
+                    del self.waypoints[0]
+                    self.determine_state()
+
+    def determine_state(self):
+        if len(self.waypoints) == 0:
+            self.state = Boy.IDLE_RIGHT if self.state == Boy.RUN_RIGHT else  Boy.IDLE_LEFT
+        else:
+            tx,ty = self.waypoints[0]
+            self.state = Boy.RUN_RIGHT if tx > self.x else Boy.RUN_LEFT
+
+
+span = 50
+def handle_events():
+    global boys
+    global span
+    events = get_events()
+    for e in events:
+        if e.type == SDL_QUIT:
+            game_framework.quit()
+        elif e.type == SDL_KEYDOWN:
+            if e.key == SDLK_ESCAPE:
+                game_framework.pop_state()
+            elif e.key in range(SDLK_1, SDLK_9 + 1):
+                span = 20 * (e.key - SDLK_0)
+
+        elif e.type == SDL_MOUSEBUTTONDOWN:
+            if e.button == SDL_BUTTON_LEFT:
+                tx, ty = e.x, 600 - e.y
+                for b in boys:
+                    bx = tx + random.randint(-span, span)
+                    by = ty + random.randint(-span, span)
+                    b.waypoints += [ (bx, by) ]
+                    b.determine_state()
+            else:
+                for b in boys:
+                    b.waypoints = []
+                    b.determine_state()
+
+def enter():
+    global boys, grass
+
+    boys = []
+    fh = open('boys_data.json', 'r')
+    data = json.load(fh)
+    for e in data['boys']:
+        b = Boy()
+        b.name = e['name']
+        b.x = e['x']
+        b.y = e['y']
+        b.speed = e['speed']
+        boys.append(b)
+
+    # boys = [ Boy() for i in range(BOYS_COUNT) ]
+    grass = Grass()
+
+
+# def main():
+#     global running
+#     enter()
+#     while running:
+#         handle_events()
+#         print(running)
+#         update()
+#         draw()
+#     exit()
+
+def draw():
+    global grass, boys
+    clear_canvas()
+    grass.draw()
+    for b in boys:
+        b.draw()
+    update_canvas()
+
+def update():
+    global boys
+    for b in boys:
+        b.update()
+    delay(0.01)
+
+# fill here
+
+def exit():
+    pass
+
+if __name__ == '__main__':
+    import sys
+    current_module = sys.modules[__name__]  
+    open_canvas()
+    game_framework.run(current_module)
+    close_canvas()
