a356254ea0b11944929a6c089d634e5545eb5d82
Kiyong Kim <scgyong@kpu.ac.kr>
2018-10-19 15:04:14 +0900

    draw/update 분기하지 않도록 수정.

--- pr1019_ctrl/boy.py
+++ pr1019_ctrl/boy.py
@@ -30,7 +30,9 @@ class Boy:
         self.y = random.randint(90, 550)
         self.speed = random.uniform(3.0, 5.0)
         self.frame = random.randint(0, 7)
-        self.state = IDLE
+        # self.state = IDLE
+        self.state = -1
+        self.set_state(IDLE)
         self.dir = 1
         self.dx = 0
         if Boy.image == None:
@@ -42,27 +44,31 @@ class Boy:
     def update_IDLE(self):
         self.frame = (self.frame + 1) % 8
 
+    def enter_RUN(self):
+        self.time = time.time()
     def draw_RUN(self):
         y = 0 if self.dir == 0 else 100
         Boy.image.clip_draw(self.frame * 100, y, 100, 100, self.x, self.y)
     def update_RUN(self):
         elapsed = time.time() - self.time
         mag = 2 if elapsed > 2.0 else 1
-        print(mag, elapsed)
+        # print(mag, elapsed)
         self.frame = (self.frame + 1) % 8
         self.x = max(25, min(self.x + mag * self.dx, 775))
 
     def draw(self):
-        if self.state == IDLE:
-            self.draw_IDLE()
-        elif self.state == RUN:
-            self.draw_RUN()
+        self.draw_func(self)
+        # if self.state == IDLE:
+        #     self.draw_IDLE()
+        # elif self.state == RUN:
+        #     self.draw_RUN()
 
     def update(self):
-        if self.state == IDLE:
-            self.update_IDLE()
-        elif self.state == RUN:
-            self.update_RUN()
+        self.update_func(self)
+        # if self.state == IDLE:
+        #     self.update_IDLE()
+        # elif self.state == RUN:
+        #     self.update_RUN()
 
     def handle_event(self, e):
         if (e.type, e.key) in key_event_table:
@@ -83,8 +89,32 @@ class Boy:
             self.set_state(IDLE if self.dx == 0 else RUN)
             # print(self.dx, self.dir)
     def set_state(self, state):
-        if self.state == IDLE and state == RUN:
-            self.time = time.time()
+        if self.state == state: return
+
+        # print(self.state, Boy.functions[self.state])
+        if self.state in Boy.functions:
+            exit = Boy.functions[self.state].get('exit', None)
+            if (exit):
+                exit(self)
 
         self.state = state
+
+        funcs = Boy.functions[self.state]
+        enter = funcs.get('enter', None)
+        if enter: enter(self)
+
+        self.draw_func = funcs['draw']
+        self.update_func = funcs['update']
+
+    functions = {
+        IDLE: {
+            'update': update_IDLE,
+            'draw': draw_IDLE
+        },
+        RUN: {
+            'enter': enter_RUN,
+            'update': update_RUN,
+            'draw': draw_RUN
+        }
+    }
 
