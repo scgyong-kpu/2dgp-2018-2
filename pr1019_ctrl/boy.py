ef31098777eabd1e6c18aaf52d3f7862d1709ef7
Kiyong Kim <scgyong@kpu.ac.kr>
2018-10-19 14:16:45 +0900

    좌/우 화살표로 움직이도록 수정.

--- pr1019_ctrl/boy.py
+++ pr1019_ctrl/boy.py
@@ -1,59 +1,60 @@
 from pico2d import *
 import random
 
+# Boy State
+IDLE, RUN = range(2)
+
+# Boy Event
+RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP = range(4)
+
+key_event_table = {
+    (SDL_KEYDOWN, SDLK_RIGHT): RIGHT_DOWN,
+    (SDL_KEYDOWN, SDLK_LEFT): LEFT_DOWN,
+    (SDL_KEYUP, SDLK_RIGHT): RIGHT_UP,
+    (SDL_KEYUP, SDLK_LEFT): LEFT_UP
+}
+
+next_state_table = {
+    IDLE: { RIGHT_UP: RUN,  LEFT_UP: RUN,  RIGHT_DOWN: RUN,  LEFT_DOWN: RUN  },
+    RUN:  { RIGHT_UP: IDLE, LEFT_UP: IDLE, RIGHT_DOWN: IDLE, LEFT_DOWN: IDLE }
+}
 class Boy:
     image = None
-    wp = None
+
     RUN_LEFT, RUN_RIGHT, IDLE_LEFT, IDLE_RIGHT = 0, 1, 2, 3
     def __init__(self):
         print("Creating..")
         # self.state = self.State.s1
         self.x = random.randint(0, 200)
         self.y = random.randint(90, 550)
-        self.speed = random.uniform(1.0, 3.0)
+        self.speed = random.uniform(3.0, 5.0)
         self.frame = random.randint(0, 7)
-        self.waypoints = []
-        self.state = Boy.IDLE_RIGHT
+        self.state = IDLE
+        self.dir = 1
+        self.dx = 0
         if Boy.image == None:
             Boy.image = load_image('../res/animation_sheet.png')
-        if Boy.wp == None:
-            Boy.wp = load_image('../res/wp.png')
     def draw(self):
-        for wp in self.waypoints:
-            Boy.wp.draw(wp[0], wp[1])
-        Boy.image.clip_draw(self.frame * 100, self.state * 100, 100, 100, self.x, self.y)
+        y = 0 if self.dir == 0 else 100
+        Boy.image.clip_draw(self.frame * 100, y, 100, 100, self.x, self.y)
     def update(self):
         self.frame = (self.frame + 1) % 8
-        if len(self.waypoints) > 0:
-            tx, ty = self.waypoints[0]
-            dx, dy = tx - self.x, ty - self.y
-            dist = math.sqrt(dx ** 2 + dy ** 2)
-            if dist > 0:
-                self.x += self.speed * dx / dist
-                self.y += self.speed * dy / dist
-
-                if dx < 0 and self.x < tx: self.x = tx
-                if dx > 0 and self.x > tx: self.x = tx
-                if dy < 0 and self.y < ty: self.y = ty
-                if dy > 0 and self.y > ty: self.y = ty
-
-                if (tx, ty) == (self.x, self.y):
-                    del self.waypoints[0]
-                    self.determine_state()
-
-    def determine_state(self):
-        if len(self.waypoints) == 0:
-            self.state = Boy.IDLE_RIGHT if self.state == Boy.RUN_RIGHT else  Boy.IDLE_LEFT
-        else:
-            tx,ty = self.waypoints[0]
-            self.state = Boy.RUN_RIGHT if tx > self.x else Boy.RUN_LEFT
+        self.x = max(25, min(self.x + self.dx, 775))
 
     def handle_event(self, e):
-        if e.type == SDL_MOUSEBUTTONDOWN:
-            if e.button == SDL_BUTTON_LEFT:
-                tx, ty = e.x, 600 - e.y
-                self.waypoints += [ (tx, ty) ]
-                self.determine_state()
-            else:
-                self.waypoints = []
-                self.determine_state()
+        if (e.type, e.key) in key_event_table:
+            key_event = key_event_table[(e.type, e.key)]
+            if key_event == RIGHT_DOWN:
+                self.dx += self.speed
+                if self.dx > 0: self.dir = 1
+            elif key_event == LEFT_DOWN:
+                self.dx -= self.speed
+                if self.dx < 0: self.dir = 0
+            elif key_event == RIGHT_UP:
+                self.dx -= self.speed
+                if self.dx < 0: self.dir = 0
+            elif key_event == LEFT_UP:
+                self.dx += self.speed
+                if self.dx > 0: self.dir = 1
+            print(self.dx, self.dir)
+
