b751dbf68c8e927a28749cd00626ca1d98cbf88a
Kiyong Kim <scgyong@kpu.ac.kr>
2018-10-29 02:36:31 +0900

    Key 받아들이기

--- pr1028_review_rot/player.py
+++ pr1028_review_rot/player.py
@@ -6,6 +6,7 @@ import time
 class Player:
     bodyImage = None
     barrelImage = None
+    interested_keys = [ SDLK_LEFT, SDLK_RIGHT, SDLK_UP, SDLK_DOWN ]
 
     def __init__(self):
         self.x = 400
@@ -13,6 +14,7 @@ class Player:
         self.angle = 0
         self.moveSpeed = 10
         self.rotSpeed = 10
+        self.keys = {}
         if Player.bodyImage == None:
             Player.bodyImage = load_image('player_body_pix.png')
         if Player.barrelImage == None:
@@ -26,5 +28,9 @@ class Player:
         pass
 
     def handle_event(self, e):
+        if e.type == SDL_KEYDOWN or e.type == SDL_KEYUP:
+            if e.key in Player.interested_keys:
+                self.keys[e.key] = e.type == SDL_KEYDOWN
+                print(e.key, e.type == SDL_KEYDOWN)
         pass
 
