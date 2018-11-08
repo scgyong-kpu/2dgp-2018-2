5fecdbee5ce70bed04c61834e6853bccb8fe0ce8
Kiyong Kim <scgyong@kpu.ac.kr>
2018-11-09 01:10:19 +0900

    q4 그리기

--- pr1106_scroll/bg.py
+++ pr1106_scroll/bg.py
@@ -30,6 +30,7 @@ class InfiniteBackground:
     def draw(self):
         self.image.clip_draw_to_origin(self.x3, self.y3, self.w3, self.h3, 0, 0)
         self.image.clip_draw_to_origin(self.x2, self.y2, self.w2, self.h2, 0, self.h3)
+        self.image.clip_draw_to_origin(self.x4, self.y4, self.w4, self.h4, self.w3, 0)
     def update(self):
         if self.target == None: 
             return
@@ -44,4 +45,9 @@ class InfiniteBackground:
         self.y2 = 0
         self.w2 = self.w3
         self.h2 = self.ch - self.h3
-        # print(self.x, self.y, self.cw, self.ch, 0, 0)
+
+        self.x4 = 0
+        self.y4 = self.y3
+        self.w4 = self.cw - self.w3 
+        self.h4 = self.h3
+
