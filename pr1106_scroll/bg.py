f730e2dfa5c86ac1ee19d99a86e0dd559dfb2007
Kiyong Kim <scgyong@kpu.ac.kr>
2018-11-09 01:07:47 +0900

    q2 그리기

--- pr1106_scroll/bg.py
+++ pr1106_scroll/bg.py
@@ -28,14 +28,20 @@ class InfiniteBackground:
         self.x, self.y = 0, 0
         self.target = None
     def draw(self):
-        self.image.clip_draw_to_origin(self.x1, self.y1, self.w1, self.h1, 0, 0)
+        self.image.clip_draw_to_origin(self.x3, self.y3, self.w3, self.h3, 0, 0)
+        self.image.clip_draw_to_origin(self.x2, self.y2, self.w2, self.h2, 0, self.h3)
     def update(self):
         if self.target == None: 
             return
         self.x = int(self.target.x - self.cw // 2)
         self.y = int(self.target.y - self.ch // 2)
-        self.x1 = self.x % self.width
-        self.y1 = self.y % self.height
-        self.w1 = self.width - self.x1
-        self.h1 = self.height - self.y1
+        self.x3 = self.x % self.width
+        self.y3 = self.y % self.height
+        self.w3 = self.width - self.x3
+        self.h3 = self.height - self.y3
+
+        self.x2 = self.x3
+        self.y2 = 0
+        self.w2 = self.w3
+        self.h2 = self.ch - self.h3
         # print(self.x, self.y, self.cw, self.ch, 0, 0)
