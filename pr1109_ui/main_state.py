caed6b025cc9b41e7ecf937431239a75a42eecff
Kiyong Kim <scgyong@kpu.ac.kr>
2018-11-13 02:07:20 +0900

    ui.py 를 재사용할 목적으로 분리하기 시작.

--- pr1109_ui/main_state.py
+++ pr1109_ui/main_state.py
@@ -1,24 +1,6 @@
 from pico2d import *
 import game_framework
-
-class Button:
-    def __init__(self, normal, selected, x, y):
-        self.normalImage = load_image(normal)
-        self.selectedImage = load_image(selected)
-        self.x, self.y = x, y
-        self.selected = False
-    def draw(self):
-        if self.selected:
-            self.selectedImage.draw(self.x, self.y)
-        else:
-            self.normalImage.draw(self.x, self.y)
-    def hits(self, x, y):
-        hw, hh = self.normalImage.w // 2, self.normalImage.h // 2
-        if x < self.x - hw: return False
-        if x > self.x + hw: return False
-        if y < self.y - hh: return False
-        if y > self.y + hw: return False
-        return True
+from ui import Button
 
 buttons = []
 def selectButton(b):
