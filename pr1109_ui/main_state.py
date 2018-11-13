ff8a200e4590f62386a188425f0adb5f9e6b841a
Kiyong Kim <scgyong@kpu.ac.kr>
2018-11-13 09:26:16 +0900

    버튼 구현.

--- pr1109_ui/main_state.py
+++ pr1109_ui/main_state.py
@@ -1,16 +1,20 @@
 from pico2d import *
 import game_framework
-from ui import Button
+import ui
 
-buttons = []
-def selectButton(b):
-    size = len(buttons)
-    for i in range(size):
-        if buttons[i] == b:
-            print(str(i) + ' has been selected')
-            buttons[i].selected = True
-        else:
-            buttons[i].selected = False
+def enter():
+    for x in [200, 400, 600]:
+        btn = ui.Button('check', x, 500)
+        ui.buttons.append(btn)
+
+def draw():
+    clear_canvas()
+    ui.draw()
+    update_canvas()
+
+def update():
+    ui.update()
+    delay(0.03)
 
 def handle_events():
     global boy
@@ -20,28 +24,7 @@ def handle_events():
             game_framework.quit()
         elif (e.type, e.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
             game_framework.pop_state()
-        elif e.type == SDL_MOUSEMOTION:
-            x, y = e.x, get_canvas_height() - e.y
-            for b in buttons:
-                if b.hits(x, y):
-                    selectButton(b)
-
-def enter():
-    global buttons
-    buttons.append(Button('check_n.png', 'check_p.png', 200, 300))
-    buttons.append(Button('check_n.png', 'check_p.png', 400, 300))
-    buttons.append(Button('check_n.png', 'check_p.png', 600, 300))
-
-def draw():
-    clear_canvas()
-    for b in buttons:
-        b.draw()
-    update_canvas()
-
-def update():
-    delay(0.03)
-
-# fill here
+        ui.handle_event(e)
 
 def exit():
     pass
