f44a4f0027b3993d49d74e39fb4eb4021073ecd0
Kiyong Kim <scgyong@kpu.ac.kr>
2018-11-20 13:02:01 +0900

    Player 추가

--- pr1120_time/main_state.py
+++ pr1120_time/main_state.py
@@ -1,36 +1,45 @@
 from pico2d import *
 import game_framework
+import game_world
 import ui
+from player import Player
 
-wants = False
-score = 0
+player = None
 
 def enter():
+    global player
     # label = ui.Label("Other color", 100, 350, 50, ui.FONT_2)
     # label.color = (255, 127, 127)
     # ui.labels.append(label)
-    pass
+    player = Player()
+    print(player)
+
 
 def draw():
     clear_canvas()
+    player.draw()
     ui.draw()
     update_canvas()
 
 def update():
     ui.update()
+    player.update()
     delay(0.03)
 
 def handle_events():
-    global boy
+    global player
     events = get_events()
     for e in events:
         if e.type == SDL_QUIT:
             game_framework.quit()
         elif (e.type, e.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
             game_framework.pop_state()
+        player.handle_event(e)
         ui.handle_event(e)
 
 def exit():
+    global player
+    del(player)
     pass
 
 if __name__ == '__main__':
---
+++ pr1120_time/player.py
@@ -0,0 +1,39 @@
+from pico2d import *
+import game_framework
+import time
+
+class Player:
+	image = None
+	RUN_SPEED_PPS = 300
+	FIELD_MARGIN = 50
+	def __init__(self):
+		self.field_width, self.field_height = get_canvas_width(), get_canvas_height()
+		self.x, self.y = self.field_width / 2, self.field_height / 2
+		self.dx, self.dy = 0, 0
+		self.speed = 1
+		self.size = 60
+		if (Player.image == None):
+			Player.image = load_image('player.png')
+	def draw(self):
+		self.image.draw(self.x, self.y)
+	def handle_event(self, event):
+		if event.type == SDL_KEYDOWN:
+			if event.key == SDLK_LEFT: self.dx += -1
+			elif event.key == SDLK_RIGHT: self.dx += 1
+			elif event.key == SDLK_UP: self.dy += 1
+			elif event.key == SDLK_DOWN: self.dy -= 1
+
+		if event.type == SDL_KEYUP:
+			if event.key == SDLK_LEFT: self.dx += 1
+			elif event.key == SDLK_RIGHT: self.dx += -1
+			elif event.key == SDLK_UP: self.dy -= 1
+			elif event.key == SDLK_DOWN: self.dy += 1
+	def update(self):
+		distance = Player.RUN_SPEED_PPS * game_framework.frame_time 
+		self.x += (self.dx * distance)
+		self.y += (self.dy * distance)
+		self.x = clamp(Player.FIELD_MARGIN, self.x, self.field_width - Player.FIELD_MARGIN) 
+		self.y = clamp(Player.FIELD_MARGIN, self.y, self.field_height - Player.FIELD_MARGIN)
+
+
+
