b6f42f7e0162ea2ee8ffa70cb1620f68628d8675
Kiyong Kim <scgyong@kpu.ac.kr>
2018-11-23 04:19:04 +0900

    composite_draw 를 사용했을 때와 비교

--- pr1120_time/player.py
+++ pr1120_time/player.py
@@ -23,8 +23,10 @@ class Player:
 		self.score = 0
 
 	def draw(self):
-		index = int(-(self.angle - math.pi / 2) * 16 / math.pi) % 32
-		self.image.clip_draw(128 * index, 0, 128, 128, self.x, self.y)
+		# index = int(-(self.angle - math.pi / 2) * 16 / math.pi) % 32
+		# self.image.clip_draw(128 * index, 0, 128, 128, self.x, self.y)
+		angle = self.angle - math.pi / 2
+		self.image.clip_composite_draw(0, 0, 128, 128, angle, '', self.x, self.y, 128, 128)
 	def handle_event(self, event):
 		handled = False
 		if event.type == SDL_KEYDOWN:
