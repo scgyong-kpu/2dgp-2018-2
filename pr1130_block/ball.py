33f6f7cded535ceced891ec2cc1a9b5352c2d79c
Kiyong Kim <scgyong@kpu.ac.kr>
2018-12-01 04:21:17 +0900

    공 움직이도록.

--- pr1130_block/ball.py
+++ pr1130_block/ball.py
@@ -1,14 +1,23 @@
+import game_framework
 from game_object import *
 
 class Ball(GameObject):
+    PPS = 100
     def __init__(self, x, y, dx, dy):
         super(Ball, self).__init__()
         self.x, self.y = x, y
         self.dx, self.dy = dx, dy
         self.size = 22
+        self.angle = 1.0
+        self.speed = 1.0
         self.image = self.init_image(Ball, 'ball.png', 3, 4)
     def update(self):
         self.update_frame()
+        distance = game_framework.frame_time * Ball.PPS * self.speed
+        dx = distance * math.cos(self.angle)
+        dy = distance * math.sin(self.angle)
+        self.x += dx
+        self.y += dy
     def draw(self):
         # print(self.frame, self._count)
         self.draw_frame()
